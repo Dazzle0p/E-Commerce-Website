8174884be443444119a37fb293c817937a9590dc
dazzle0p <dazushant@gmail.com>
2025-08-05 01:16:06 +0530

    Small fix

--- frontend/src/componenets/Layout/Hero.jsx
+++ frontend/src/componenets/Layout/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import heroImg from "../../assets/rabbit-hero.webp";
+import heroImg from "../../assets/heroimg.jpg";
 import { Link } from "react-router-dom";
 const Hero = () => {
   return (
