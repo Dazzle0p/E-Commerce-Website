f57e13d32253386b7d88b1cb310cac0c6c0d8430
dazzle0p <dazushant@gmail.com>
2025-08-07 15:07:25 +0530

    Fix : hero iamge path

--- frontend/src/componenets/Layout/Hero.jsx
+++ frontend/src/componenets/Layout/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import heroImg from "../../../assets/heroimg.jpg";
+import heroImg from "../../assets/heroimg.jpg";
 import { Link } from "react-router-dom";
 const Hero = () => {
   return (
